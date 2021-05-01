eabcac3f1dafbbf39e710ecddab1b069b45fbbba
Weichi Zhao <wzhao42@asu.edu>
2021-05-01 20:24:15 +0800

    5/1

---
+++ src/main/java/getKthFromEnd.java
@@ -0,0 +1,30 @@
+import java.util.List;
+
+public class getKthFromEnd {
+
+    //Definition for singly-linked list.
+    public static class ListNode {
+        int val;
+        ListNode next;
+        ListNode(int x) { val = x; }
+    }
+    public ListNode getKthFromEnd(ListNode head, int k) {
+        ListNode former = head, latter = head;
+        for(int i = 0; i < k; i++)
+            former = former.next;
+        while(former != null){
+            former = former.next;
+            latter = latter.next;
+        }
+        return latter;
+    }
+    public static void main(String[] args) {
+        ListNode input = new ListNode(1);
+        input.next = new ListNode(2);
+        input.next.next = new ListNode(3);
+        input.next.next.next = new ListNode(4);
+        input.next.next.next.next = new ListNode(5);
+        getKthFromEnd obj = new getKthFromEnd();
+        obj.getKthFromEnd(input, 2);
+    }
+}
---
+++ src/main/java/levelOrder.java
@@ -0,0 +1,39 @@
+import java.util.*;
+
+public class levelOrder {
+
+    //Definition for a binary tree node.
+    public static class TreeNode {
+      int val;
+      TreeNode left;
+      TreeNode right;
+      TreeNode(int x) { val = x; }
+    }
+
+    public int[] levelOrder(TreeNode root){
+        if(root == null) return new int[0];
+        List<Integer> res = new ArrayList<>();
+        Queue<TreeNode> queue = new LinkedList<>(){{ add(root); }};
+        while(!queue.isEmpty()){
+            TreeNode node = queue.poll();
+            res.add(node.val);
+            if(node.left != null) queue.add(node.left);
+            if(node.right != null) queue.add(node.right);
+        }
+        int[] tmp = new int[res.size()];
+        for(int i = 0; i < res.size(); i++)
+            tmp[i] = res.get(i);
+        return tmp;
+    }
+
+    public static void main(String[] args) {
+        levelOrder obj = new levelOrder();
+        TreeNode input = new TreeNode(3);
+        input.left = new TreeNode(9);
+        input.right = new TreeNode(20);
+        input.right.left = new TreeNode(15);
+        input.right.right = new TreeNode(7);
+        int[] res = obj.levelOrder(input);
+        System.out.println();
+    }
+}
