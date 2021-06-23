d057568977154c95ac7030937d943d4b949e80a3
Weichi Zhao <wzhao42@asu.edu>
2021-06-23 14:56:12 +0800

    u2

--- src/main/java/alibaba/ipTest.java
+++ src/main/java/alibaba/ipTest.java
@@ -9,6 +9,7 @@ public class ipTest {
     }
     public static boolean isInRange(String ip, String cidr) {
         String[] ips = ip.split("\\.");
+        //把ip地址转换成二进制
         int test = Integer.parseInt(ips[0]) << 24;
         int test1 = Integer.parseInt(ips[1]) << 16;
         int test2 = Integer.parseInt(ips[2]) << 8;
@@ -18,14 +19,15 @@ public class ipTest {
                 | (Integer.parseInt(ips[1]) << 16)
                 | (Integer.parseInt(ips[2]) << 8) | Integer.parseInt(ips[3]);
         int type = Integer.parseInt(cidr.replaceAll(".*/", ""));
-        int mask = 0xFFFFFFFF << (32 - type);
         String cidrIp = cidr.replaceAll("/.*", "");
         String[] cidrIps = cidrIp.split("\\.");
+        //把网段转换成二进制
         int cidrIpAddr = (Integer.parseInt(cidrIps[0]) << 24)
                 | (Integer.parseInt(cidrIps[1]) << 16)
                 | (Integer.parseInt(cidrIps[2]) << 8)
                 | Integer.parseInt(cidrIps[3]);
 
+        int mask = 0xFFFFFFFF << (32 - type);
         return (ipAddr & mask) == (cidrIpAddr & mask);
     }
 
