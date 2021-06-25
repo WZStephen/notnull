6a2f4d32c12f12562fe67097010fbebfe5af59bb
Weichi Zhao <wzhao42@asu.edu>
2021-06-25 15:35:16 +0800

    zzz

--- src/main/java/alibaba/printMax_recursively.java
+++ src/main/java/alibaba/printMax_recursively.java
@@ -1,19 +1,16 @@
 package alibaba;
 
 public class printMax_recursively {
-    public int max(int[] arr, int n){
-        if(n <= 0)
-            return arr[0];
-        if(arr[n] > arr[n-1]){
-            return arr[n];
-        } else{
-            return max(arr, n-1);
+    public int findMax(int[] arr, int index){
+        if(index == arr.length-1){
+            return arr[index];
         }
+        return Math.max(arr[index], findMax(arr,index+1));
     }
 
     public static void main(String[] args) {
         int[] arr = new int[]{4,5,3,2};
         printMax_recursively obj = new printMax_recursively();
-        System.out.println(obj.max(arr, 3));
+        System.out.println(obj.findMax(arr, 0));
     }
 }
